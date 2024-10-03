6fae45f46c99a38e333ada2758bc9156b0e5667d
Qinan Wang <qwang75@jhu.edu>
2024-10-03 14:26:20 -0400

    added a function in setTESSref.py to get the TESSvector information can be run by 'python setTESSref.py {sector no} {camera no} --dir {directory name}'

--- setTESSref.py
+++ setTESSref.py
@@ -7,14 +7,17 @@ Created on Sat Sep 14 12:35:45 2024
 """
 
 import argparse
+import tessvectors as tv
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test1', type=int, default=None, help='BLAAAAAA')
-
+    #parser.add_argument('--test1', type=int, default=None, help='BLAAAAAA')
+    parser.add_argument('--filetype', type=str, default='FFI')
+    parser.add_argument('--dir', type=str, default='./')
+    parser.add_argument('sector', type=int)
+    parser.add_argument('camera', type=int)
     args = parser.parse_args()
-
-    for x in range(1,100):
-        print(f'HELLO {x}')
+    vector = tv.getvector((args.filetype, args.sector, args.camera))
+    vector.to_csv('{0}TessVectors_S{1:0=3d}_C{2}_FFI.csv'.format(args.dir, args.sector, str(args.camera)))
         
     print('STEP setTESSref FINISHED')
